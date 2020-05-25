071d2abdf5d40d4a240f072b3fe1ebf20152db48
Dimmy <ddimmy@narod.ru>
2020-05-25 22:12:27 +0300

    Результат домашнего задания №5

---
+++ src/ru/geakbrains/lessons/Employee.java
@@ -0,0 +1,51 @@
+package ru.geakbrains.lessons;
+
+// 1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+public class Employee {
+    private String family;
+    private String name;
+    private String secondName;
+    private String position;
+    private String email;
+    private String phone;
+    private float salary;
+    private int age;
+
+    //2. Конструктор класса должен заполнять эти поля при создании объекта
+    public Employee(String family,
+                  String name,
+                  String secondName,
+                  String position,
+                  String email,
+                  String phone,
+                  float salary,
+                  int age){
+        this.family = family;
+        this.name = name;
+        this.secondName = secondName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    //3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль
+    public void printConsole(){
+        System.out.println(family+"   "+
+                name+"   "+
+                secondName+"    "+
+                position+"   "+
+                email+"   "+
+                phone+"   "+
+                salary+"   "+
+                age);
+    }
+}
---
+++ src/ru/geakbrains/lessons/Lesson5.java
@@ -0,0 +1,35 @@
+package ru.geakbrains.lessons;
+
+public class Lesson5 {
+    public static void main(String[] args){
+        int quantity = 5;
+        //4. Создать массив из 5 сотрудников
+        Employee[] persons = new Employee[quantity];
+
+        persons[0] = new Employee("Иванов", "Иван", "Иванович",
+                "Директор", "IvanovII@gb.ru", "+7 (916) 111-11-11",
+                100000, 50);
+        persons[1] = new Employee("Петров", "Петр", "Петрович",
+                "Маркетолог", "PetrovPP@gb.ru", "+7 (916) 111-11-12",
+                25000, 31);
+        persons[2] = new Employee("Сидоров", "Сидр", "Сидорович",
+                "Инженер", "SidorovSS@gb.ru", "+7 (916) 111-11-13",
+                20000, 41);
+        persons[3] = new Employee("Маринина", "Мария", "Ивановна",
+                "Бухгалтер", "MarininaMI@gb.ru", "+7 (916) 111-11-14",
+                50000, 25);
+        persons[4] = new Employee("Томаторина", "Гульнара", "Махмудовна",
+                "Уборщик", "TomatorinaGM@gb.ru", "+7 (916) 111-11-15",
+                15000, 47);
+        printEmployee(persons,40);
+    }
+
+    //5. С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
+    static void printEmployee(Employee[] a, int age){
+        for(int i=0;i<a.length;i++){
+            if(a[i].getAge()>age) {
+                a[i].printConsole();
+            }
+        }
+    }
+}
