4936720117a478549a63598d2d474662c134cf32
Dimmy <ddimmy@narod.ru>
2020-07-06 20:14:59 +0300

    Домашнее задание №5  - доработал задание с учетом возможности задания нескольких потоков.

--- src/ru/geakbrains/level1/Lesson4/Lesson4.java
+++ src/ru/geakbrains/level1/Lesson4/Lesson4.java
@@ -9,8 +9,10 @@ public class Lesson4 {
     public static void main(String[] arg) {
         System.out.println("Result of method #1");
         method1();
-        System.out.println("\nResult of method #2");
-        method2();
+        System.out.println("\nResult of method #2 (from 1 to 5 thread)");
+        for (int i = 1; i < 6; i++) {
+            method2(i);
+        }
     }
 
     public static void method1(){
@@ -19,53 +21,55 @@ public class Lesson4 {
         fillArray(arr,1);
         long startTime;
         startTime = currentTime();
-        countArray(arr);
+        countArray(arr, 0);
 //        5) Проверяется время окончания метода System.currentTimeMillis().
 //        6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a).
         System.out.println("Time is: "+(currentTime()-startTime)+" (ms).");
     }
 
-    public static void method2(){
+    public static void method2(int numberThred){
 //1) Создают одномерный длинный массив
         float[] arr = new float[SIZE];
         fillArray(arr,1);
         long startTime;
+        int partSize = SIZE/numberThred;
         startTime = currentTime();
-        float[] arr1 = new float[HALF];
-        float[] arr2 = new float[HALF];
-        System.arraycopy(arr, 0, arr1, 0, HALF);
-        System.arraycopy(arr, HALF, arr2, 0, HALF);
-
-        Thread t1= new Thread(new Runnable() {
-            @Override
-            public void run() {
-                countArray(arr1);
+        float[][] arr1=new float[numberThred][partSize+SIZE%numberThred];
+        Thread t[] = new Thread[numberThred];
+        int i;
+        for (i = 0; i < numberThred; i++) {
+            if(i == numberThred-1) {
+                System.arraycopy(arr, i * partSize, arr1[i], 0, partSize+SIZE % numberThred);
             }
-        });
-
-        Thread t2= new Thread(new Runnable() {
-            @Override
-            public void run() {
-                countArray(arr2);
+            else{
+                System.arraycopy(arr, i * partSize, arr1[i], 0, partSize);
             }
-        });
-
-        t1.start();
-        t2.start();
-
-        try{
-            t1.join();
-            t2.join();
-        }catch (InterruptedException e){
-            e.printStackTrace();
+            final int n=i;
+            t[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    countArray(arr1[n],n*partSize);
+                }
+            });
         }
 
-        System.arraycopy(arr1,0,arr,0, HALF);
-        System.arraycopy(arr2,0,arr,HALF, HALF);
+        for (Thread a: t){ a.start();}
+
+        for (Thread a: t){
+            try {
+                a.join();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        for (i = 0; i < numberThred; i++) {
+            System.arraycopy(arr1[i],0,arr,i*partSize, arr1[i].length);
+        }
 
 //        5) Проверяется время окончания метода System.currentTimeMillis().
 //        6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a).
-        System.out.println("Time is: "+(currentTime()-startTime)+" (ms).");
+        System.out.println("Number of thread is "+numberThred+" time is: "+(currentTime()-startTime)+" (ms).");
     }
 
 //2) Заполняют этот массив единицами.
@@ -81,9 +85,9 @@ public class Lesson4 {
     }
 
 //4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
-    public static void countArray(float[] arr){
+    public static void countArray(float[] arr, int number){
         for (int i =0;i<arr.length;i++) {
-            arr[i]=(float)(arr[i]*Math.sin(0.2f+i/5)*Math.cos(0.2f+i/5)*Math.cos(0.4f+i/2));
+            arr[i]=(float)(arr[i]*Math.sin(0.2f+(i+number)/5)*Math.cos(0.2f+(i+number)/5)*Math.cos(0.4f+(i+number)/2));
         }
     }
 }
